383af6232cd50ec5153dc84e8e578318b88b29e0
Devins11 <devinfcb42@gmail.com>
2025-06-21 23:39:26 +0700

    added controller

---
+++ src/main/java/com/app/controller/CommitController.java
@@ -0,0 +1,28 @@
+package com.app.controller;
+
+import com.app.model.GithubWebhook;
+import com.app.service.NotionSyncService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class CommitController {
+
+    @Autowired
+    private NotionSyncService notionSyncService;
+
+    @PostMapping("")
+    public ResponseEntity<String> handleWebhook(@RequestBody GithubWebhook githubWebhook){
+        notionSyncService.syncToNotion();
+
+        return ResponseEntity.ok().build();
+    }
+
+
+
+}
