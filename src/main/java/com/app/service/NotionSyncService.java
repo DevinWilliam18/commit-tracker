fea39c866c75153db35551f95a756670323a81bf
Devins11 <devinfcb42@gmail.com>
2025-06-29 23:14:49 +0700

    added request body

--- src/main/java/com/app/service/NotionSyncService.java
+++ src/main/java/com/app/service/NotionSyncService.java
@@ -1,4 +1,20 @@
 package com.app.service;
 
+import com.app.model.NotionSync;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
 public interface NotionSyncService {
+
+    List<NotionSync> getSynedToNotionFalse();
+
+    void syncToNotion();
+
+    void pushToNotion(NotionSync notionSync);
+
+    Map<String, Object> dataToNotion(NotionSync notionSync);
+
 }
