6ce206895b47b29630860e2949f78601cb4ab0e1
Devins11 <devinfcb42@gmail.com>
2025-08-18 23:35:44 +0700

    added @Repository annotation

--- src/main/java/com/app/dao/CommitDao.java
+++ src/main/java/com/app/dao/CommitDao.java
@@ -2,9 +2,11 @@ package com.app.dao;
 
 import com.app.model.NotionSync;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommitDao extends MongoRepository<NotionSync, String> {
     List<NotionSync> findBySyncedToNotionFalse();
 
