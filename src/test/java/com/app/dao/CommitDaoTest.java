a92b61193da160f08d6ee7f49903705372e8d70f
Devins11 <devinfcb42@gmail.com>
2025-10-12 22:59:39 +0700

    test(unittest): modified unit testing for repository - adding logger

--- src/test/java/com/app/dao/CommitDaoTest.java
+++ src/test/java/com/app/dao/CommitDaoTest.java
@@ -1,9 +1,11 @@
 package com.app.dao;
 
 import com.app.model.NotionSync;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -11,8 +13,12 @@ import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@DataMongoTest
+@Slf4j
 class CommitDaoTest {
+
+    @Autowired
+    private MongoProperties mongoProperties;
 
     @Autowired
     private CommitDao commitDao;
@@ -23,7 +29,7 @@ class CommitDaoTest {
         notionSync1.setId("test123");
         notionSync1.setNotionDatabaseId("1234asad");
         notionSync1.setPlatform("Leetcode");
-        notionSync1.setTitle("Coin Change");
+        notionSync1.setTitle("Coin   Change");
         notionSync1.setTopic("DP");
         notionSync1.setDifficulty("Hard");
         notionSync1.setPath("https://leetcode.com");
@@ -32,16 +38,18 @@ class CommitDaoTest {
         notionSync1.setUpdatedAt(ZonedDateTime.now(ZoneId.of("Asia/Jakarta")).toInstant());
 
         NotionSync notionSync2 = new NotionSync();
-        notionSync1.setId("test323");
-        notionSync1.setNotionDatabaseId("1200asad");
-        notionSync1.setPlatform("Hackerrank");
-        notionSync1.setTitle("Coin Change");
-        notionSync1.setTopic("DP");
-        notionSync1.setDifficulty("Hard");
-        notionSync1.setPath("https://leetcode.com");
-        notionSync1.setSyncedToNotion(false);
-        notionSync1.setCreatedAt(ZonedDateTime.now(ZoneId.of("Asia/Jakarta")).toInstant());
-        notionSync1.setUpdatedAt(ZonedDateTime.now(ZoneId.of("Asia/Jakarta")).toInstant());
+        notionSync2.setId("test323");
+        notionSync2.setNotionDatabaseId("1200asad");
+        notionSync2.setPlatform("Hackerrank");
+        notionSync2.setTitle("Coin Change");
+        notionSync2.setTopic("DP");
+        notionSync2.setDifficulty("Hard");
+        notionSync2.setPath("https://leetcode.com");
+        notionSync2.setSyncedToNotion(false);
+        notionSync2.setCreatedAt(ZonedDateTime.now(ZoneId.of("Asia/Jakarta")).toInstant());
+        notionSync2.setUpdatedAt(ZonedDateTime.now(ZoneId.of("Asia/Jakarta")).toInstant());
+
+        log.info("mongodb: {}", mongoProperties.getUri());
 
         commitDao.saveAll(Arrays.asList(notionSync1, notionSync2));
 
